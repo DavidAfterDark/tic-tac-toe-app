814bcde882cf4b64eb67f4a50878f4a22b41d87f
David <hanktech1.0@gmail.com>
2021-11-24 12:04:09 -0400

    create alert component

---
+++ src/components/AlertModal.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { View, Text, StyleSheet, Modal } from 'react-native'
+
+//  components
+import Buttom from './Buttom'
+
+const AlertModal = ({ visible, message, button, onPressButton }) => {
+  return (
+    <Modal
+      visible={visible}
+      transparent
+      statusBarTranslucent
+    >
+      <View style={styles.container}>
+        <View style={styles.body}>
+          <Text style={[styles.message, button && { marginBottom: 20 }]}>{message}</Text>
+          {button && <Buttom onPress={onPressButton} text='Restart' />}
+        </View>
+      </View>
+    </Modal>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)'
+  },
+
+  body: {
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 10
+  },
+
+  message: {
+    color: '#000',
+    fontSize: 22
+  }
+})
+
+export default AlertModal
