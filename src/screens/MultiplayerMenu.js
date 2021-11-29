430e94f9a76f8dc925c6c748fd954f3ff1f73793
HankTech <hnk.inc1.0@gmail.com>
2021-11-28 23:48:40 -0400

    created multiplayer menu

---
+++ src/screens/MultiplayerMenu.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+
+//  components
+import BackgroundContainer from '../components/BackgroundContainer'
+import Buttom from '../components/Buttom'
+
+const MultiplayerMenu = () => {
+  const navigation = useNavigation()
+
+  const goLocalMode = () => navigation.navigate('Game')
+
+  return (
+    <BackgroundContainer>
+      <View style={styles.container}>
+        <Buttom
+          onPress={goLocalMode}
+          text='Local'
+          buttomStyle={styles.buttomStyle}
+          textStyle={styles.buttonTextStyles}
+        />
+        <Buttom
+          onPress={() => console.log('coming soon')}
+          text='Online'
+          buttomStyle={styles.buttomStyle}
+          textStyle={styles.buttonTextStyles}
+        />
+      </View>
+    </BackgroundContainer>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center'
+  },
+
+  buttomStyle: {
+    height: 50,
+    marginTop: 25,
+    marginBottom: 28
+  },
+
+  buttonTextStyles: {
+    fontSize: 20
+  }
+})
+
+export default MultiplayerMenu
