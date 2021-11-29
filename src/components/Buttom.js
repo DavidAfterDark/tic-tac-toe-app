8555e6edb6cd8c79a4e3e38d906502aaeb104529
HankTech <hnk.inc1.0@gmail.com>
2021-11-28 21:46:07 -0400

    extend button styles

--- src/components/Buttom.js
+++ src/components/Buttom.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Text, TouchableOpacity, StyleSheet } from 'react-native'
 
-const Buttom = ({ onPress, text }) => {
+const Buttom = ({ onPress, text, textStyle, buttomStyle }) => {
   return (
-    <TouchableOpacity onPress={onPress} style={styles.container}>
-      <Text style={[styles.text]}>{text}</Text>
+    <TouchableOpacity onPress={onPress} style={[styles.container, buttomStyle]}>
+      <Text style={[styles.text, textStyle]}>{text}</Text>
     </TouchableOpacity>
   )
 }
@@ -13,7 +13,8 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: 'rgba(31, 86, 123, 0.8)',
     padding: 5,
-    borderRadius: 10
+    borderRadius: 10,
+    justifyContent: 'center'
   },
 
   text: {
