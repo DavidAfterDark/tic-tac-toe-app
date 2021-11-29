e3acf410d9e1c7edd47aa91afb2fe631639ff1a7
HankTech <hnk.inc1.0@gmail.com>
2021-11-28 21:46:44 -0400

    add new constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,10 @@
+//  -----   game type   -----
+export const SINGLE_PLAYER = 'SINGLE_PLAYER'
+export const MULTI_PLAYER = 'MULTI_PLAYER'
+
+//  ----    game mode   ----
 export const LOCAL = 'LOCAL'
+export const ONLINE = 'ONLINE'
 
 //  -----   bot dificulty   -----
 export const BOT_EASY = 'BOT_EASY'
